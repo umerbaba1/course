917f725b0b5ff46c0a69d024e9c7a7f588b8b084
umerbaba1 <umerbab567@gmail.com>
2024-12-12 21:44:12 +0530

    Fixed routes

--- routes/course.js
+++ routes/course.js
@@ -1,24 +1,55 @@
-const { Router}=require("express")
-const CourseRoute=Router()
+const { Router } = require("express");
+const CourseRoute = Router();
+const { CourseModel, PurchaseModel } = require("../db");
+const { UserMiddleware } = require("../middleware/user");
 
-CourseRoute.get('/',(req,res)=>{
+CourseRoute.get("/", async (req, res) => {
+    const allCourse=await CourseModel.find({})
     res.json({
-        msg:"all courses"
+        allCourse
     })
-})
+});
 
-CourseRoute.post('/purchase',(req,res)=>{
+CourseRoute.post("/purchase", UserMiddleware, async (req, res) => {
+  const Id=req.userId
+  const courseId=req.body.courseId
+
+  //fix if user had already bought the course
+  const UserPurchases=  await PurchaseModel.find({
+    userId:Id,
+    courseId
+  })
+  console.log(UserPurchases)
+  if(UserPurchases.length==1){
     res.json({
-        msg:"Buy some course"
+        message:"You had already bought the course "
     })
-})
+  }else{
+    await PurchaseModel.create({
+        userId:Id,
+        courseId
+      })
+      res.json({
+        message:"Course Purchased"
+      })
+  }
+  
+});
 
-CourseRoute.get('/purchases',(req,res)=>{
-    res.json({
-        msg:"Courses you have bought"
-    })
-})
+CourseRoute.get("/purchases",UserMiddleware,async (req, res) => {
+ const userId=req.userId
+ const allPurchases= await PurchaseModel.find({
+    userId
+ })
+ const CourseDetail=await CourseModel.find({
+    _id:{$in : allPurchases.map(x=> x.courseId)}
+ })
+ res.json({
+    allPurchases,
+    CourseDetail
+ })
+});
 
-module.exports={
-    CourseRoute:CourseRoute
-}
+module.exports = {
+  CourseRoute: CourseRoute,
+};
